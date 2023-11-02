261fcd8395c8ab5955e9eaaef56027ec73725f92
Remy Wang <remy.wang727@gmail.com>
2023-11-02 20:12:35 +0100

    Migrate Shopify Polaris version from v11 to v12 (#12)
    * Migrate Shopify Polaris version from v11 to v12
    
    * Keep spacing in TextContainer
    
    * Fix: keep spacing variant

--- web/frontend/pages/ExitIframe.jsx
+++ web/frontend/pages/ExitIframe.jsx
@@ -35,7 +35,7 @@ export default function ExitIframe() {
       <Layout>
         <Layout.Section>
           <div style={{ marginTop: "100px" }}>
-            <Banner title="Redirecting outside of Shopify" status="warning">
+            <Banner title="Redirecting outside of Shopify" tone="warning">
               Apps can only use /exitiframe to reach Shopify or the app itself.
             </Banner>
           </div>
